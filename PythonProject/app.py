846308e2b3a53dbdcaa95926bbe7e03cf87ce3c1
PQ07 <96368618+PQ07@users.noreply.github.com>
2025-05-16 20:47:09 +0530

    Update app.py

--- PythonProject/app.py
+++ PythonProject/app.py
@@ -69,4 +69,4 @@ def index():
     return render_template('index.html', prediction=label, image_path=result_file)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
